8e87a7462ee5b172eeb5179ba06e3bde050ec567
Austin Dryden <austinisnorobot@gmail.com>
2020-01-02 15:55:29 -0500

    both parts completetd

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const http = require("http");
 const fs = require("fs");
-let htmlFile = fs.readFileSync("madlib.html").toString();
+
+const htmlFile = fs.readFileSync("madlib.html").toString();
+
 console.log(htmlFile);
 let server = http.createServer((req, res) =>{
-    let name = req.url.slice(1).split(".")[0];
+    let url = req.url.slice(1).split("/");
+    let name = url[0];
+    let place = "";
+    if (url.length > 0){
+        place = url[1];
+    }
     console.log(name);    
-
-
-
+    res.writeHead(200,{
+        "Content-Type" : "text/html"
+    })
+    let newHTML = htmlFile.replace("***NAME***", name).replace("***PLACE***", place);
+    
+    res.end(newHTML);
 });
 
 server.listen(3000, () => console.log("we outchea"));
